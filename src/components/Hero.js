e7693073ca15bbd30fecdfdd9fdbddc7e578c451
Rahul Roy <rahulroy.ec21@rvce.edu.in>
2023-11-27 22:48:08 +0530

    update

---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import bg from '../assets/bg.png';
+
+const Hero = () => {
+    return (
+        <div className="bg-fixed h-screen text-4xl text-lime-900 font-bold" style={{backgroundImage: `url(${bg})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
+            <div className="text-6xl self-center mx-auto">
+            <h1 className="mt-6 ">Welcome to Indian Farm Co.</h1>
+            <p className="mt-16">Discover the goodness of organic jaggery</p>
+            <button>Shop Now</button>
+            </div>         
+        </div>
+    );
+};
+
+export default Hero;
---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    
+         <nav className="fixed flex flex-row-reverse w-full top-0 left-0 bg-white">
+             <div className="mx-6 text-xl font-bold">
+            <a href="/" className="text-lime-700 hover:text-gray-500">Portfolio</a>
+        </div>    
+        <div className="mx-6 text-xl font-bold">
+            <a href="/" className="text-lime-700 hover:text-gray-500">Contact Us</a>
+        </div>   
+        <div className="mx-6 text-xl font-bold">
+            <a href="/" className="text-lime-700 hover:text-gray-500">About Us</a>
+        </div>  
+        <div className="mx-6 text-xl font-bold">
+            <a href="/" className="text-lime-700 hover:text-gray-500">Home</a>
+        </div>  
+        
+    </nav>
+
+   
+  )
+}
+
+export default Navbar
